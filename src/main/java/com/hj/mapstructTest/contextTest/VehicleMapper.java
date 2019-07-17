e701ca891b0a4615d8b36f2d90c3e4b29e2d269e
huangjie <huangjie@gaosiedu.com>
2019-07-17 17:23:08 +0800

    fix

---
+++ src/main/java/com/hj/mapstructTest/contextTest/VehicleMapper.java
@@ -0,0 +1,12 @@
+package com.hj.mapstructTest.contextTest;
+
+import org.mapstruct.Mapper;
+
+/**
+ * @author huangjie
+ * @time 2019-07-17 17:10
+ */
+@Mapper
+public abstract class VehicleMapper {
+
+}
